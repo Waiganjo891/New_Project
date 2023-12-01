94b0e1f5ad34ab47ad7996af716940e39737d759
Waiganjo <waiganjothiongo12@gmail.com>
2023-11-30 23:08:09 -0800

    Fix 1

--- prompt.c
+++ prompt.c
@@ -1,22 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
-int main()
+#define MAX_COMMAND 1024
+
+void prompt(char **av, char **env)
 {
-	char *buffer;
-	size_t bufsize = 1024;
+	char *string = NULL;
+	int i, j, status;
+	size_t n = 0;
+	ssize_t num_char;
+	char *argv[MAX_COMMAND];
+	pid_t child_pid;
 
-	buffer = (char *)malloc(bufsize * sizeof(char));
-	if (buffer == NULL)
+	while (1)
 	{
-		perror("Unable to allocate buffer");
-		exit(1);
+		if (isatty(STDIN_FILENO))
+			printf("$ ");
+		num_char = getline(&string, &n, stdin);
+		if (num_char == -1 || num_char == EOF)
+		{
+			free(string);
+			break;
+		}
+		i = 0;
+		while (string[i])
+		{
+			if (string[i] == '\n')
+				string[i] = 0;
+			i++;
+		}
+		j = 0;
+		argv[j] = strtok(string, " ");
+		while (argv[j])
+		{
+			argv[++j] = strtok(NULL, " ");
+		}
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			free(string);
+			exit(EXIT_FAILURE);
+		}
+		else if (child_pid == 0)
+		{
+			if (execve(argv[0], argv, env) == -1)
+			{
+				printf("%s: No such file or directory\n", av[0]);
+			}
+		}
+		else
+		{
+			wait(&status);
+		}
+	};
+}
+
+int main(int ac, char **av, char **env)
+{
+	if (ac == 1)
+	{
+		prompt(av, env);
 	}
-	printf("$ ");
-	getline(&buffer, &bufsize, stdin);
-	buffer[strcspn(buffer, "\n")] = 0;
-	printf("%s\n", buffer);
-	free(buffer);
-	return (0);
 }
